a76f0c94a508a32fb31b494161408465cfdac767
WHITESPINE <jakehenry.tn@gmail.com>
2025-08-12 17:09:35 +0000

    Calendar integration

---
+++ src/integrations/calendar/calendar.js
@@ -0,0 +1,12 @@
+import configuration from "./configuration.json"; 
+import events from "./events.json";
+
+export async function initCalendar() {
+    // Do base init
+    await SimpleCalendar.api.configureCalendar(configuration);
+
+    // Add every event
+    for (let evt of events) {
+        await SimpleCalendar.api.addNote(evt.title, evt.content, evt.startDate, evt.endDate, evt.allDay, evt.repeats, evt.categories, null, null, evt.userVisibility, evt.remindUsers);
+    }
+}
