65c097f7e65040f01ac2e6a29f287223340b6b0a
WHITESPINE <jakehenry.tn@gmail.com>
2025-08-12 00:43:56 +0000

    HMR example

--- src/system.js
+++ src/system.js
@@ -7,3 +7,11 @@ Hooks.once('init', async function() {
 Hooks.once('ready', async function() {
 
 });
+
+// HMR reload of various components
+if (import.meta.hot) {
+  import.meta.hot.accept("./hooks/hotbar.mjs", module => {
+    Hooks.off("hotbarDrop", hot_hooks.hotbar);
+    hot_hooks.hotbar = Hooks.on("hotbarDrop", module.onHotbarDrop);
+  });
+}
