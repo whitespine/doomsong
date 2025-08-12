ece52c5cad69adc16f5cf23a46801a0a210e232c
WHITESPINE <jakehenry.tn@gmail.com>
2025-08-12 21:38:47 +0000

    Fixed calendar init

--- src/integrations/calendar/calendar.js
+++ src/integrations/calendar/calendar.js
@@ -7,6 +7,6 @@ export async function initCalendar() {
 
     // Add every event
     for (let evt of events) {
-        await SimpleCalendar.api.addNote(evt.title, evt.content, evt.startDate, evt.endDate, evt.allDay, evt.repeats, evt.categories, null, null, evt.userVisibility, evt.remindUsers);
+        await SimpleCalendar.api.addNote(evt.title, evt.content, evt.startDate, evt.endDate, evt.allDay, evt.repeats, evt.categories, undefined, undefined, evt.userVisibility, evt.remindUsers);
     }
 }
