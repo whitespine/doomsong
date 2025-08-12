ddb700623fbd475056f387f3989765e030b32a2a
WHITESPINE <jakehenry.tn@gmail.com>
2025-08-12 20:17:41 +0000

    Small tweak

--- src/system.mjs
+++ src/system.mjs
@@ -27,8 +27,10 @@ Hooks.once('ready', async function() {
 
 // HMR reload of various components
 if (import.meta.hot) {
+  /*
   import.meta.hot.accept("./hooks/hotbar.mjs", module => {
     Hooks.off("hotbarDrop", hot_hooks.hotbar);
     hot_hooks.hotbar = Hooks.on("hotbarDrop", module.onHotbarDrop);
   });
+  */
 }
