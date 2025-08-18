80f8e6bccf4de3f123cb19c6625112955877cfd3
WHITESPINE <jakehenry.tn@gmail.com>
2025-08-18 20:45:23 +0000

    Sheet reflexivity and portrait edits

--- src/components/sheets/NPCSheet.svelte
+++ src/components/sheets/NPCSheet.svelte
@@ -3,4 +3,43 @@
     $inspect(props);
 </script>
 
-<span>Hi</span>
+<div class="npc-sheet">
+    <img class="portrait" src={props.data.img} alt="potrait" onclick={() => props.app.editImage("img")}>
+    <div class="stats">
+
+    </div>
+    <div class="moves">
+        <span>Moves</span>
+    </div>
+
+    <div class="bio">
+        <span>Description</span>
+    </div>
+</div>
+
+<style lang="scss">
+    .npc-sheet {
+        display: grid;
+        grid-template:
+            "p s" 128px
+            "m m" 1fr
+            "b b" 1fr / 128px 1fr;
+
+        .portrait {
+            grid-area: p;
+            border: 1px solid black;
+        }
+
+        .stats {
+            grid-area: s;
+        }
+
+        .moves {
+            grid-area: m;
+        }
+
+        .bio {
+            grid-area: b;
+        }
+    }
+</style>
--- src/sheets/ActorSheet.js
+++ src/sheets/ActorSheet.js
@@ -17,4 +17,34 @@ export class DoomsongActorSheet extends ActorSheet {
     get componentToMount() {
         throw new TypeError("Do not instantiate this class directly");
     }
+
+
+    // Stolen from foundry, modified
+    async editImage(attr) {
+        const current = foundry.utils.getProperty(this.document._source, attr);
+        const defaultArtwork = this.document.constructor.getDefaultArtwork?.(this.document._source) ?? {};
+        const defaultImage = foundry.utils.getProperty(defaultArtwork, attr);
+        // const fp = new FilePicker.implementation({ // v13
+        const fp = new FilePicker({
+            current,
+            type: "image",
+            redirectToRoot: defaultImage ? [defaultImage] : [],
+            callback: path => {
+                this.document.update({
+                    [attr]: path
+                });
+            },
+            position: {
+                top: this.position.top + 40,
+                left: this.position.left + 10
+            }
+        });
+        await fp.browse();
+    }
+
+    async getData(options={}) {
+        let base = await super.getData();
+        base.app = this;
+        return base;
+    }
 }
