f2c5e47a196cd7b7bb1496179199417728c666d7
WHITESPINE <jakehenry.tn@gmail.com>
2025-08-18 21:26:25 +0000

    Basic fields

---
+++ src/components/fields/UpdateInput.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { resolveDotpath } from "../../utils/paths";
+    let {
+        document,
+        path,
+        debounce = 200,
+        class: className,
+        ...restProps
+    } = $props();
+
+    let cv = $derived(resolveDotpath(document, path, ""));
+
+    function onChange(evt) {
+        cv = evt.target.value;
+        update_callback();
+    }
+
+    let update_callback = foundry.utils.debounce(() => {
+        document.update({
+            [path]: cv,
+        });
+    }, debounce);
+</script>
+
+<input
+    onchange={onChange}
+    oninput={onChange}
+    {...restProps}
+    class={className}
+    value={cv}
+/>
--- src/components/sheets/NPCSheet.svelte
+++ src/components/sheets/NPCSheet.svelte
@@ -1,15 +1,41 @@
 <script>
+    import UpdateTextField from "../fields/UpdateInput.svelte";
+    import Dice from "../rolls/Dice.svelte";
     let props = $props();
     $inspect(props);
 </script>
 
 <div class="npc-sheet">
-    <img class="portrait" src={props.data.img} alt="potrait" onclick={() => props.app.editImage("img")}>
+    <img
+        class="portrait"
+        src={props.data.img}
+        alt="potrait"
+        onclick={() => props.app.editImage("img")}
+    />
     <div class="stats">
-
+        {#snippet field(key, label, path)}
+        <div>
+            <label for={key}>{label}:</label>
+            <UpdateTextField name={key} document={props.actor} path={path} type="text" />
+        </div>
+        {/snippet}
+        {@render field("name", "Name", "name")}
+        {@render field("max_toughness", "Max Toughness", "system.max_toughness")}
+        {@render field("toughness", "Toughness", "system.toughness")}
+        {@render field("protection", "Protection", "system.protection")}
+        {@render field("max_footing", "Max Footing", "system.max_footing")}
+        {@render field("footing", "Footing", "system.footing")}
     </div>
     <div class="moves">
         <span>Moves</span>
+        {#each [1,2,3,4,5,6] as act}
+        <div class="move-options">
+            <Dice value={act} style="width: 32px; height: 32px" />
+            <div class="flexcol">
+
+            </div>
+        </div>
+        {/each}
     </div>
 
     <div class="bio">
@@ -32,10 +58,17 @@
 
         .stats {
             grid-area: s;
+            display: grid;
+            grid-template: 1fr / repeat(3, 1fr);
         }
 
         .moves {
             grid-area: m;
+
+            .move-options {
+                display: flex;
+                flex-direction: row;
+            }
         }
 
         .bio {
