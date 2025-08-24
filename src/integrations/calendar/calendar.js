d292d80f98547a103f90c904c56f021c8f61ec0a
WHITESPINE <jakehenry.tn@gmail.com>
2025-08-24 15:36:40 +0000

    Pruned events.json

--- src/integrations/calendar/calendar.js
+++ src/integrations/calendar/calendar.js
@@ -7,6 +7,16 @@ export async function initCalendar() {
 
     // Add every event
     for (let evt of events) {
-        await SimpleCalendar.api.addNote(evt.title, evt.content, evt.startDate, evt.endDate, evt.allDay, evt.repeats, evt.categories, undefined, undefined, evt.userVisibility, evt.remindUsers);
+        await SimpleCalendar.api.addNote(
+            evt.title, 
+            evt.content, 
+            evt.startDate, 
+            evt.endDate || evt.startDate, // Default to a full day on the same day 
+            evt.allDay || true, // Default to an all day event
+            evt.repeats || 3, // Default to repeating yearly
+            evt.categories || [], // Default no calendar
+            undefined, undefined, 
+            evt.userVisibility ? (Array.isArray(evt.userVisibility) ? evt.userVisibility : [evt.userVisibility]) : [], // Coerce to array. Default to empty. "default" allows players to see too
+            evt.remindUsers);
     }
 }
