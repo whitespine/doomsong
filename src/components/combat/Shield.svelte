6e9f1979cde0cf0eeac529f435a50442e0a32feb
WHITESPINE <jakehenry.tn@gmail.com>
2025-10-07 20:11:44 +0000

    Scalable shield

--- src/components/combat/Shield.svelte
+++ src/components/combat/Shield.svelte
@@ -1,6 +1,6 @@
 <script>
     import UpdateInput from "../fields/UpdateInput.svelte";
-    import {resolveDotpath} from "../../utils/paths";
+    import { resolveDotpath } from "../../utils/paths";
     let { actor } = $props();
 </script>
 
@@ -9,45 +9,39 @@
         <UpdateInput
             name={path}
             doc={actor}
-            path={path}
+            {path}
             value={resolveDotpath(actor, path, 0)}
             type="number"
+            style="height: 40cqh"
         />
     {/each}
 </div>
 
 <style lang="scss" module>
     .shield {
-        --shield-width: 100px;
-        --shield-height: 120px;
-        --top-padding: 24px;
-        --left-padding: 10px;
+        container-type: size;
 
-        padding-top: var(--top-padding);
-        padding-left: var(--left-padding);
-        padding-right: 30px;
-        padding-bottom: 20px;
+        padding-top: 28%;
+        padding-left: 5%;
+        padding-right: 30%;
+        padding-bottom: 12%;
 
-        min-width: var(--shield-width);
-        max-width: var(--shield-width);
-        min-height: var(--shield-height);
-        max-height: var(--shield-height);
+        aspect-ratio: 489 / 545;
         background-image: url("$assets/misc/StatusShield.png");
-        background-size: 100% auto;
+        background-size: contain;
         background-repeat: no-repeat;
 
         display: grid;
         grid-template: 1fr 1fr / 1fr 1fr;
 
         input {
-            max-width: 25px;
+            // max-width: 25px;
             text-align: center;
-            // margin-top: 15px;
-            margin-bottom: 0px;
             background: transparent;
             border: none;
             padding: 0px;
             color: black;
+            font-size: 30cqh;
         }
     }
 </style>
